5ebf1d6c0a3ac176b825ffc0163a15e0df25ec2a
Matthew <mn3040@nyu.edu>
2025-02-15 19:56:31 -0500

    adding hw 1

--- Project 1 Simple 2D Scene/Project 1 Simple 2D Scene/main.cpp
+++ Project 1 Simple 2D Scene/Project 1 Simple 2D Scene/main.cpp
@@ -14,6 +14,16 @@
 #include "ShaderProgram.h"
 #include "stb_image.h"
 
+/**
+* Author: Matthew Nunez
+* Assignment: Simple 2D Scene
+* Date due: 2025-02-15, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 enum AppStatus { RUNNING, TERMINATED };
 
 constexpr int WINDOW_WIDTH = 640,
